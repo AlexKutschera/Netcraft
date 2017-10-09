b21b1fe1991521f30d025c75d56237a9bb9a0d33
Alexander Kuschera <alexkutschera01@gmail.com>
2017-10-09 18:57:28 +0200

    Updated

--- src/main/java/netcraft/Netcraft.java
+++ src/main/java/netcraft/Netcraft.java
@@ -3,13 +3,22 @@ package netcraft;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.ModMetadata;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Reference.MOD_ID, version = Reference.MOD_VERSION)
 public class Netcraft {
+
+    private ModMetadata metadata;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+        metadata = event.getModMetadata();
+    }
     
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+        System.out.println(metadata.modId);
     }
 }
