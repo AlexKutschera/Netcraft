4cce276d548215bc5b22784211cb1c76eb11437c
Alexander <alexanderalbers01@gmail.com>
2017-10-13 18:37:28 +0200

    Update Blocks

--- src/main/java/netcraft/init/Blocks.java
+++ src/main/java/netcraft/init/Blocks.java
@@ -1,4 +1,23 @@
 package netcraft.init;
 
+import net.minecraft.block.Block;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+
 public class Blocks {
+
+    public static  void init(){
+
+    }
+    public static  void register(){
+
+    }
+    public static  void registerRenders(){
+
+    }
+    public static void registerBlock(Block block) {
+        ForgeRegistries.BLOCKS.register(block);
+    }
+    public static void registerRender(Block block){
+
+    }
 }
