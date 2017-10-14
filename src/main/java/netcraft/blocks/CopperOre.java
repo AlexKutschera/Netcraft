944943107059e7f0ee7255b1a6e4afaef13d5259
Alexander Kuschera <alexkutschera01@gmail.com>
2017-10-14 12:34:32 +0200

    /Copper Ore /cuprit_block

--- src/main/java/netcraft/blocks/CopperOre.java
+++ src/main/java/netcraft/blocks/CopperOre.java
@@ -2,11 +2,7 @@ package netcraft.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.item.Item;
 import netcraft.Netcraft;
-
-import java.util.Random;
 
 public class CopperOre extends Block {
 
