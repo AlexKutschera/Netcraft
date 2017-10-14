d109018c5ba6dee9d987d9f8b9924ce27700a069
Alexander <alexanderalbers01@gmail.com>
2017-10-15 00:14:59 +0200

    Update Blocks and Items

--- src/main/java/netcraft/items/FirstDisk.java
+++ src/main/java/netcraft/items/FirstDisk.java
@@ -3,13 +3,15 @@ package netcraft.items;
 import net.minecraft.item.Item;
 import netcraft.Netcraft;
 
-public class FirstDisk extends Item {
+public class FirstDisk extends Item
+{
 
     //Todo Platine für Crafting?
 
     private static final String name = "first_disk";
 
-    public FirstDisk(){
+    public FirstDisk()
+    {
         setRegistryName(name);
         setUnlocalizedName(name);
         setCreativeTab(Netcraft.NETCRAFT_TAB);
